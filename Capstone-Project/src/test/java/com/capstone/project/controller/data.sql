b2fc61c87126450dbc8e9f7456ff70500bd8ee9c
prem54 <44597991+prem54@users.noreply.github.com>
2019-12-27 00:07:47 +0530

    Delete data.sql
    unused file

--- Capstone-Project/src/test/java/com/capstone/project/controller/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO TBL_EMPLOYEEDETAILS (empId, empName, emailId, departmentName) VALUES 
-(101, 'Lokesh', 'abc@gmail.com', 'IT'),
-(102, 'Deja', 'xyz@email.com', 'IT'),
-(103, 'Caption', 'cap@marvel.com', 'IT');
